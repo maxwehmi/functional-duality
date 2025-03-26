fb3c889da6b3b80ed3740a17df56811ff41ba6a1
Giacomo Bruno Ruggero <giacomobrdeantonellis@gmail.com>
2025-03-26 16:47:38 +0100

    commit1

---
+++ lib/prettyprinting.hs
@@ -0,0 +1,13 @@
+
+module Prettyprinting where
+    import Poset
+    import qualified Data.Set as Set 
+    import qualified Data.Maybe as M
+
+
+    import Data.GraphViz.Types.Monadic
+    import Data.GraphViz.Types.Generalised
+    import Data.GraphViz.Commands
+
+
+
